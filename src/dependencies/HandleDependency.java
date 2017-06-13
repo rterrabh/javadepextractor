b4a89fa75d15d8e9ccfd06a186e250d58af728fe
Arthur Ferreira <arthurfp@sistemas.ufla.br>
2017-06-13 13:53:15 -0300

    Removed Duplicated Dependencies

---
+++ src/dependencies/HandleDependency.java
@@ -0,0 +1,10 @@
+package dependencies;
+
+
+public abstract class HandleDependency extends Dependency {
+
+	protected HandleDependency(String classNameA, String classNameB, Integer lineNumberA, Integer offset, Integer length) {
+		super(classNameA, classNameB, lineNumberA, offset, length);
+	}
+	
+}
